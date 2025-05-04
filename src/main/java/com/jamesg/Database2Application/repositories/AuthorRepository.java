b06ed38cb25b72929813a76950453744b8dbe75f
James Gonzalez <jamesg8025@gmail.com>
2025-05-04 05:03:11 -0500

    Setup Author repository and its integration tests

--- src/main/java/com/jamesg/Database2Application/repositories/AuthorRepository.java
+++ src/main/java/com/jamesg/Database2Application/repositories/AuthorRepository.java
@@ -1,4 +1,12 @@
 package com.jamesg.Database2Application.repositories;
 
-public interface AuthorRepository {
+// Spring JPA provides the interface for us, so we don't need to implement it - if we extend the correct repository interface
+
+
+import com.jamesg.Database2Application.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Similar to @Component, makes this a Spring bean
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
